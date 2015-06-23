bca4b728053883a9fecaf46e218e3499d20e6351
mrc0mmand <frantisek.sumsal@gmail.com>
2015-06-23 13:48:40 +0200

    Module mod_currency now supports BTC

--- botmodules/mod_currency.py
+++ botmodules/mod_currency.py
@@ -5,11 +5,11 @@
 from . import module_base
 import urllib.request
 import time
+import json
 import sys
 import re
 
 # TODO:
-# BTC
 # Doge
 
 class Currency(module_base.ModuleBase):
@@ -29,6 +29,7 @@ class Currency(module_base.ModuleBase):
 
         rc = 0
         rc += self._get_curr_CNB()
+        rc += self._get_curr_BTC()
 
         if rc != 0:
             self._last_update -= 1800
@@ -40,7 +41,7 @@ class Currency(module_base.ModuleBase):
             req = urllib.request.urlopen('http://www.cnb.cz/cs/financni_trhy/devizovy_trh/'
                                          'kurzy_devizoveho_trhu/denni_kurz.txt', None, 5)
         except Exception as e:
-            print('[Currency] Couldn\'t fetch currency rates for CNB.', file=sys.stderr)
+            print('[Currency] Couldn\'t fetch currency rates for CNB {}' .format(e), file=sys.stderr)
             return 1
         
         for line in req:
@@ -48,6 +49,28 @@ class Currency(module_base.ModuleBase):
             if m:
                 self._currency_data[m.group(2)] = dict(amount=int(m.group(1)), 
                                                        rate=float(m.group(3).replace(',', '.')))
+
+        return 0
+
+    def _get_curr_BTC(self):
+        if not self._currency_data['USD']:
+            print('[Currency] Can\'t fetch currency rate for BTC - missing USD rate.', file=sys.stderr)
+            return 1
+
+        try:
+            req = urllib.request.urlopen('http://blockchain.info/ticker', None, 5)
+            content = json.loads(req.read().decode('utf-8'))
+        except Exception as e:
+            print('[Currency] Couldn\'t fetch currency rate for BTC: {}' .format(e), file=sys.stderr)
+            return 1
+        
+        if content['USD'] and content['USD']['15m']:
+            self._currency_data['BTC'] = dict(rate=(content['USD']['15m'] * 
+                                              self._currency_data['USD']['rate']), amount=1)
+        else:
+            print('[Currency] Couldn\'t fetch currency rate for BTC - probably changed JSON format?',
+                  file=sys.stderr)
+            return 1
 
         return 0
 
@@ -76,10 +99,11 @@ class Currency(module_base.ModuleBase):
 
             if m:
                 source = float(m.group(1).replace(',', '.'))
-                converted = round(self._convert(m.group(2).upper(), m.group(4).upper(), source), 2)
+                converted = self._convert(m.group(2).upper(), m.group(4).upper(), source)
 
-                self.send_msg(connection, event, is_public, '{:,.2f} {} = {:,.2f} {}' 
-                              .format(round(source, 2), m.group(2).upper(), converted, m.group(4).upper()))
+                self.send_msg(connection, event, is_public, '{:,} {} = {:,} {}' 
+                              .format(round(source, 4), m.group(2).upper(), 
+                                      round(converted, 4), m.group(4).upper()))
             else:
                 self.send_msg(connection, event, is_public, 
                               'Usage: {0}{1} xx.x CUR (in|to) CUR [type {0}{1}-list for '
