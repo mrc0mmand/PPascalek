c340c25e0775de9ae0ad19ca2af6ad8679694616
Jakub Mach <jakub.m4ch@gmail.com>
2015-07-15 19:29:56 +0200

    added support for wikipedias other than english

--- botmodules/mod_wiki.py
+++ botmodules/mod_wiki.py
@@ -12,18 +12,18 @@ class Wiki(module_base.ModuleBase):
     def __init__(self, settings):
         # language codes used for determing which wikipedia to grab the article from, defaults to english
         # currently only contains wikipedias that have above 100,000 articles
-        self.language_codes = ["en","sv","de","nl","fr","war","ru","ceb","it","es","vi","pl","ja","pt","zh","uk","ca","fa","sh","no","ar","fi","id","ro","cs","hu","sr","ko","ms","tr","min","eo","kk","eu","da","sk","bg","he","hy","lt","hr","sl","et","uz","gl","nn","vo","la","simple","el","hi"] 
-
-
+        language_codes = ["en","sv","de","nl","fr","war","ru","ceb","it","es","vi","pl","ja","pt","zh","uk","ca","fa","sh","no","ar","fi","id","ro","cs","hu","sr","ko","ms","tr","min","eo","kk","eu","da","sk","bg","he","hy","lt","hr","sl","et","uz","gl","nn","vo","la","simple","el","hi"] 
+        self.regex = re.compile("^((en|sv|de|simple|" + "|".join(language_codes) + "|)\s)?(.*)$") # jsem retard no, nasrat :^)
+        
     def get_commands(self):
         return ['wiki', 'wikipedia']
 
-    def _get_wiki_result(self, word):
+    def _get_wiki_result(self, langcode, word):
         try:
             # quote by měl bejt v py3 fixnutej na unikód, jestli neni tak rip
-            req = urllib.request.urlopen("https://en.wikipedia.org/w/api.php?"
-                  "action=opensearch&format=json&search={0}"
-                  .format(urllib.request.quote(word)), None, 5) 
+            req = urllib.request.urlopen("https://{0}.wikipedia.org/w/api.php?"
+                  "action=opensearch&format=json&search={1}"
+                  .format(urllib.request.quote(langcode), urllib.request.quote(word)), None, 5)  # dunno why I am quoting the langcode, but just to be sure or someshit, I am autistic so what would you expect :^)
         except URLError as e:
             return "[Wiki] Nothing found." 
         except Exception as e:
@@ -31,19 +31,22 @@ class Wiki(module_base.ModuleBase):
                   .format(str(e)), file=sys.stderr)
             return "[Wiki] Unknown error."
 
-        # .read() vrací nějaký mrdkobajty, proto decode utf-8, zasranej python3
         parsed = json.loads(req.read().decode("utf-8"))
-
         if parsed[1] and parsed[2] and parsed[3]:
             article = parsed[1][0]
-            shortinfo = parsed[2][0] if parsed[2][0] != "" else "No short description available"
+            shortinfo = parsed[2][0] if parsed[2][0] != "" else "No short description available."
             url = parsed[3][0]
             return "[Wiki] {0}: {1} ({2})".format(article, shortinfo, url)
         else:
             return "[Wiki] Nothing found."
 
     def on_command(self, module_data, connection, event, is_public):
-        args = event.arguments[0] # change this when an argument system gets implemented
-        #to_where = event.target if is_public == True else event.source
-        self.send_msg(connection, event, is_public, self._get_wiki_result(args))
-        
+        args     = event.arguments[0]
+        langcode = "en"    
+        match    = re.match(self.regex, args)
+        if(match is not None):
+            langcode    = match.group(2) or "en"
+            word        = match.group(3)
+            self.send_msg(connection, event, is_public, self._get_wiki_result(langcode, word))
+        else:
+            self.send_msg(connection, event, is_public, "[Wiki] Usage: ?wiki [langcode] <query>") # [] jsou optional argumenty v manualech, right? I hope so
