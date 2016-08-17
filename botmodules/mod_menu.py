2a1bfc90b3124e3ccc9fee0f08be7908221c5569
Frantisek Sumsal <frantisek@sumsal.cz>
2016-08-17 10:45:25 +0200

    Fix mod_menu

--- botmodules/mod_menu.py
+++ botmodules/mod_menu.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from . import module_base
 import urllib.request
+from . import module_base
 from urllib.error import URLError
 
 class Menu(module_base.ModuleBase):
 
     def __init__(self, b, settings):
+        self._url = "http://vps2.blaskovic.sk:8099/?menu="
         pass
-
-    def get_commands(self):
-        return [ 'menu' ]
 
     def _get_menu(self, b, connection, event, is_public, word):
         try:
-            req = urllib.request.urlopen("http://blaskovic.sk:8099/?menu={0}".format(urllib.request.quote(word)), None, 5)
+            req = urllib.request.urlopen(self._url + "{}".format(
+                urllib.request.quote(word)), None, 5)
         except URLError as e:
             b.send_msg(connection, event, is_public, "[Menu_mod] 404")
             return
         except Exception as e:
-            b.send_msg(connection, event, is_public, "[Menu_mod] Unknown error: " + str(e))
+            b.send_msg(connection, event, is_public,
+                    "[Menu_mod] Unknown error: " + str(e))
             return
 
         out = req.read().decode("utf-8")
@@ -29,7 +29,9 @@ class Menu(module_base.ModuleBase):
 
         return out
 
+    def get_commands(self):
+        return [ 'menu' ]
+
     def on_command(self, b, module_data, connection, event, is_public):
         args = event.arguments[0]
-
         self._get_menu(b, connection, event, is_public, args)
